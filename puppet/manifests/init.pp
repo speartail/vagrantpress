ec104e0dce1acf82ef21f33f853a461be84b4d96
Peter Hoeg <peter@speartail.com>
2014-12-01 19:08:01 +0800

    disable the bits we do not need

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -3,11 +3,11 @@ exec { 'apt_update':
   path    => '/usr/bin'
 }
 
-class { 'git::install': }
-class { 'subversion::install': }
+# class { 'git::install': }
+# class { 'subversion::install': }
 class { 'apache2::install': }
 class { 'php5::install': }
 class { 'mysql::install': }
-class { 'wordpress::install': }
-class { 'phpmyadmin::install': }
-class { 'phpqa::install': }
+# class { 'wordpress::install': }
+# class { 'phpmyadmin::install': }
+# class { 'phpqa::install': }
